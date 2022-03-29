d22c22ea2685b46e758d9f7cef386c48bd6e9059
haticekarakoyun <hatice.karakoyun9323@gmail.com>
2022-03-29 13:59:04 -0400

    Add git ignore file

--- src/Homework/Home.java
+++ src/Homework/Home.java
@@ -3,5 +3,8 @@ package Homework;
 public class Home {
     public static void main(String[] args) {
         System.out.println("Hello homework");
+        System.out.println("Hello homework");
+        System.out.println("Hello homework");
+        System.out.println("Hello homework");
     }
 }
